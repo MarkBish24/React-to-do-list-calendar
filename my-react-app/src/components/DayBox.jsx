b2b6b63c9c53c87e876d86c48cf7d9bc8efba549
MarkBish24 <markbish24@gmail.com>
2025-04-03 12:07:10 -0700

    added some modifications

--- my-react-app/src/components/DayBox.jsx
+++ my-react-app/src/components/DayBox.jsx
@@ -3,8 +3,8 @@ import "./DayBox.css";
 export default function DayBox() {
   return (
     <div className="day-box">
-      <span>12</span>
-      <button>+</button>
+      <span className="day-text">12</span>
+      <button className="note-button">+</button>
       <div className="note-container"></div>
     </div>
   );
