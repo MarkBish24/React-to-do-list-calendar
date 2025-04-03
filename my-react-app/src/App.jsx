a97078bfa30803fdcb44208410a992f6d1fc9d45
MarkBish24 <markbish24@gmail.com>
2025-04-02 18:38:57 -0700

    completed boxes in calendar and other updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner.jsx";
 import DayBanner from "./components/DayBanner.jsx";
+import DayBox from "./components/DayBox.jsx";
 
 function App() {
   return (
     <>
       <Banner />
       <DayBanner />
-      <div className="calendar"></div>
+      <div className="calendar">
+        <DayBox />
+        <DayBox />
+        <DayBox />
+        <DayBox />
+        <DayBox />
+        <DayBox />
+        <DayBox />
+      </div>
     </>
   );
 }
---
+++ my-react-app/src/components/DayBox.jsx
@@ -0,0 +1,11 @@
+import "./DayBox.css";
+
+export default function DayBox() {
+  return (
+    <div className="day-box">
+      <span>12</span>
+      <button>+</button>
+      <div className="note-container"></div>
+    </div>
+  );
+}
