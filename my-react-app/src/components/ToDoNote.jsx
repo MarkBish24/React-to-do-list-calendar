5898d2ff4579eed9fccdb103b503596e239fef99
MarkBish24 <markbish24@gmail.com>
2025-04-04 12:33:29 -0700

    added colors to notes

--- my-react-app/src/components/ToDoNote.jsx
+++ my-react-app/src/components/ToDoNote.jsx
@@ -1,5 +1,30 @@
 import "./ToDoNote.css";
 
 export default function ToDoNote({ entry }) {
-  return <div className="to-do-note"> {entry} </div>;
+  const randomColor = setRandomColor();
+  return (
+    <div
+      className="to-do-note"
+      style={{
+        backgroundColor: `${randomColor}CC`,
+        border: `3px solid ${randomColor}`,
+      }}
+    >
+      {entry}
+    </div>
+  );
+}
+
+const colors = [
+  "#FBE7C6",
+  "#B4F8C8",
+  "#A0E7E5",
+  "#FFAEBC",
+  "#FF8A65",
+  "#B39DDB",
+];
+
+function setRandomColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
 }
